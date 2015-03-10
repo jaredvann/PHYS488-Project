55eb4ca57f41fbc322d2c1e01f09c2b9e001cde7
Jared <jared@vann.me>
2015-03-10 16:02:30 +0000

    Added Particle, Point & Vector classes

---
+++ Point.java
@@ -0,0 +1,9 @@
+
+class Point {
+	double x, y;
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+}
---
+++ Vector.java
@@ -0,0 +1,9 @@
+
+class Vector {
+	double x, y;
+
+	public Vector(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+}
