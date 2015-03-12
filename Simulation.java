1f120a077742436ab3d952302f9ab9cc82106f83
Tom Spooner <spooner10000@gmail.com>
2015-03-12 19:48:05 +0000

    Updated Simulation.java and config/main.properties
    Added some simulation parameters like the number of particles and
    min/max muon momentums. Also added a Config object in the Simulation
    class to load the parameters at runtime.

--- Simulation.java
+++ Simulation.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
  */
 public class Simulation {
     private static final PrintWriter screen = new PrintWriter(System.out);
+    private static final Config config = new Config("main");
 
     private static CoincidenceDetector cd;
 
@@ -27,17 +28,19 @@ public class Simulation {
     }
 
     public static void main(String[] args) {
-        // I've hard-coded the parameters in for now...
-
         // Set up the Coincidence Detector (it needs the radii)
-        cd = new CoincidenceDetector(90, 91);
+        cd = new CoincidenceDetector(
+            config.getDouble("coincidenceDetectorRadiusA"),
+            config.getDouble("coincidenceDetectorRadiusA"));
 
         // How many muons should we simulate?
         int count = 1;
 
         // Initialize the muon array and get a new MuonFactory instance
         muons = new double[count][2];
-        factory = new MuonFactory(125, 1000);
+        factory = new MuonFactory(
+            config.getDouble("minMomentum"),
+            config.getDouble("maxMomentum"));
 
         // Run a simulation for each of the muons
         for (int i = 0; i < count; i++) {
