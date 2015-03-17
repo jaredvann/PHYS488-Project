7c69148aa552326a5167e31371a4464982a22f8e
Jonathan Barrett <jonny.barrett@hotmail.com>
2015-03-17 10:57:49 +0000

    Updates to Muon Factory

--- MuonFactory.java
+++ MuonFactory.java
@@ -1,40 +1,37 @@
 import java.io.*;
 import java.util.Random;
-class MuonFactory{
+public class MuonFactory{
 
-  static BufferedReader keyboard = new BufferedReader (new InputStreamReader(System.in)) ;
-  static PrintWriter screen = new PrintWriter( System.out, true);
   static Random value = new Random();
   static int underflow;
-  static double age = 0
-  static Point position = 0
-  static double mass =106
+  double age = 0;
+  Point position = 0;
+  double mass =106;
+  double minP ;
+  double maxP ;
+  double nextP ;
 
-  Config config = new Config("Muon");
-
-  public class MuonFactory(){
-  return muon;
+  public MuonFactory(double minP, double maxP)  {
+      this.minP = minP;
+      this.maxP = maxP;
   }
 
     /*random angle in radians generated and returned*/
-
-    public static double muonAngle()
-    {
+    public double muonAngle()  {
         double nextangle;
         nextangle = value.nextDouble()*Math.PI*2;
         return nextangle;
     }
-    /*Random Momentum in MeV/c generated and returned*/
 
-    public static double muonMomentum(){
-        double nextP;
-        double MaxP = config.getDouble("MaxP");
-        double MinP = config.getDouble("MinP");
+    /*Random Momentum in MeV/c generated and returned*/
+    public double muonMomentum() {
         nextP =value.nextDouble()*(MaxP-MinP)+MinP;
         return nextP;
         }
 
-    static Particle muon = new particle(mass, nextP, position, nextangle, age);
-    return muon;
-
+    //double[] = {theta, momentum}
+    public double[] newmuon()  {
+        double [2] muon ={muonAngle() ,muonMomentum() };
+        return muon;
+    }
   }
