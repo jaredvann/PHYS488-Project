fb4a6765f4433afe709c76971e6acbc29e666b9e
Jared Vann <j@vann.me>
2015-03-18 14:01:38 +0000

    Added detector layer logic to Simulation

--- Simulation.java
+++ Simulation.java
@@ -15,6 +15,8 @@ public class Simulation {
 
     private static CoincidenceDetector cd;
 
+    private static ArrayList<Layer> layers = new ArrayList<Layer>();
+
     private static double[] masses;
     private static ParticleFactory factory;
     private static double[][] muons;
@@ -42,6 +44,22 @@ public class Simulation {
 
         // Set up the Coincidence Detector (it needs the radii)
         cd = new CoincidenceDetector();
+
+        // Add beam pipe layer
+        layers.add(new BeamPipe("Beam Pipe", 35, 3));
+
+        // Radii of silicon detector layers
+        double[] sdr = {45, 80, 120, 180, 300, 400, 500, 700, 900, 910};
+
+        // Add silicon detector layers
+		for (int i = 0; i < sdr.length; i++) {
+			layers.add(
+                new SiliconDetector("S-"+String.valueOf(sdr[i]), sdr[i], 0.5)
+            );
+        }
+
+        // Add additional vacuum layers and sort into correct order
+	    setup();
 
         // How many muons should we simulate?
         int count = config.getInt("numParticles");
@@ -72,8 +90,8 @@ public class Simulation {
             momenta.writeToDisk("./tmp.csv");
 
             // Send the muon through all the layers in the accelerator
-            // for (Layer layer : layers)
-            //     muon = layer.handle(muon);
+            for (Layer layer : layers)
+                muon = layer.handle(muon);
 
             screen.println("[*] Actual momentum: " + muon[1]);
 
@@ -91,4 +109,25 @@ public class Simulation {
             }
         }
     }
+
+    public void setup() {
+		ArrayList<Layer> layers2 = new ArrayList<Layer>();
+		Double last_z = 0.0;
+
+		for (Layer l : layers) {
+			if (l.getDistance() > last_z) {
+				layers2.add(new VacuumLayer("V-"+String.valueOf(last_z), last_z, l.getDistance()));
+				last_z = l.getDistance() + l.getThickness();
+			}
+		}
+
+		layers.addAll(layers2);
+
+		layers.sort(new Comparator<Layer>() {
+			@Override
+			public int compare(Layer l1, Layer l2) {
+				return ((Double) l1.getDistance()).compareTo(l2.getDistance());
+			}
+		});
+	}
 }
